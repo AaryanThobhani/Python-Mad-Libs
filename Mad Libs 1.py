b8b2ca268bb6b25d4e0712b6222a43f4196e7037
Aaryan Thobhani <64651700+AaryanThobhani@users.noreply.github.com>
2022-01-09 15:37:14 -0500

    Create Mad Libs 1.py

---
+++ Mad Libs 1.py
@@ -0,0 +1,17 @@
+# Program for getting the year of birth
+# By Aaryan Thobhani
+# GitHub:  https://github.com/AaryanThobhani
+
+# variables
+user_person = input("Insert a name: ")
+user_verb = input("Inert a verb: ")
+user_noun = input("Insert a park name Ex(Queen's): ")
+user_color = input("Insert a color: ")
+user_money = input("Enter a number greater than 1: ")
+user_police_name = input("Insert a name starting with Officer: ")
+
+# Print Statement
+print("As {} was {} at {} Park, under the {} flowers, he saw ${}, he immediately gave the money to {}, and the officer said he was a good person.".format(
+    user_person, user_verb, user_noun, user_color, user_money, user_police_name))
+
+# End of Program
